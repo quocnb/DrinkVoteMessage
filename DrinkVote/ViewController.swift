2150ec13a815689f3106a1042981fc406a4980b3
Quoc Nguyen <quocnb.vnu@gmail.com>
2018-10-25 14:58:45 +0900

    Initial Commit

---
+++ DrinkVote/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DrinkVote
+//
+//  Created by Quoc Nguyen on 2018/10/25.
+//  Copyright © 2018 Quoc Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
